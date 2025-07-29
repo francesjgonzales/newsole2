12fde501cec0cb6f95d604bef222dcd6bc430b84
francesjgonzales <francesj7837@gmail.com>
2025-07-28 23:29:24 -0600

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ["newsole2.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
