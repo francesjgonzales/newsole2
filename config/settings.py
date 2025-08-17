5bfb9e216e31080e4265c7ebee3a60a4156024a5
francesjgonzales <francesj7837@gmail.com>
2025-08-17 16:51:57 -0600

    fixing requirement packages

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ['newsole2.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['newsole2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
